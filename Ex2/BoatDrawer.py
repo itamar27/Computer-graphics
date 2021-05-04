3aa0dc303e20535c1ff4604cae6a214f7227b55d
barakdaniel <barakdaniel22@gmail.com>
2021-05-04 17:56:31 +0300

    Added scaling
    Now the painting loaded from the txt file is getting normalized from the origin vector and then scales to the window size.

--- Ex2/BoatDrawer.py
+++ Ex2/BoatDrawer.py
@@ -20,7 +20,6 @@ helv36 = tkFont.Font(family='Helvetica', size=10, weight='bold')
 
 # Functions
 
-
 def createCoordinates(string):
     '''
     Parse the string from file with the token splitting the arguments
@@ -29,13 +28,58 @@ def createCoordinates(string):
     coor = string.split(',')
     return coor
 
+def scaleInputToScreen(lines, radiuses, curves):
+    '''
+    Getting a vector representation of a painting, then scales it to the actual board size.
+    '''
+    maxWidth = 0
+    maxHeight = 0
+    
+    for line in lines:
+        x1, y1, x2, y2 = createCoordinates(line)
+        if(max(int(x1),int(x2)) > maxWidth):
+            maxWidth = max(int(x1),int(x2))
+        if(max(int(y1),int(y2)) > maxHeight):
+            maxHeight = max(int(y1),int(y2))
+        
+    for radius in radiuses:
+        x1, y1, r, size = createCoordinates(radius)
+        if(int(x1)+int(size) > maxWidth):
+            maxWidth = int(x1)+int(size)
+        if(int(y1)+int(size) > maxHeight):
+            maxHeight = int(y1)+int(size)
+
+    for curve in curves:
+        x1, y1, x2, y2, x3, y3, x4, y4 = createCoordinates(curve)
+        if(max(int(x1),int(x2), int(x3), int(x4)) > maxWidth):
+            maxWidth = max(int(x1),int(x2), int(x3), int(x4))
+        if(max(int(y1),int(y2), int(y3),int(y4)) > maxHeight):
+            maxHeight = max(int(y1),int(y2), int(y3),int(y4))
+    
+    newLines = []
+    newRadiuses = []
+    newCurves = []
+
+    for line in lines:
+        x1, y1, x2, y2 = createCoordinates(line)
+        newLines.append([int(x1)/maxWidth*(width-200), int(y1)/maxHeight*height, int(x2)/maxWidth*(width-200), int(y2)/maxHeight*height])
+
+    for radius in radiuses:
+         x1, y1, r, size = createCoordinates(radius)
+         newRadiuses.append([int(x1)/maxWidth*(width-200), int(y1)/maxHeight*height, int(size)/((maxHeight+maxWidth)/2)*((height+(width-200))/2)])
+    
+    for curve in curves:
+        x1, y1, x2, y2, x3, y3, x4, y4 = createCoordinates(curve)
+        newCurves.append([int(x1)/maxWidth*(width-200), int(y1)/maxHeight*height, int(x2)/maxWidth*(width-200), int(y2)/maxHeight*height, int(x3)/maxWidth*(width-200), int(y3)/maxHeight*height, int(x4)/maxWidth*(width-200), int(y4)/maxHeight*height])
+        
+    return newLines, newRadiuses, newCurves
 
 def drawLines(lines):
     '''
     receive all lines as a list and add them to the canvas
     '''
     for line in lines:
-        x1, y1, x2, y2 = createCoordinates(line)
+        x1, y1, x2, y2 = line
         MyLine(int(x1), int(y1), int(x2), int(y2), canvas)
 
 
@@ -44,7 +88,7 @@ def drawRadiuses(radiuses):
     receive all curves as list and add them to the canvas
     '''
     for radius in radiuses:
-        x1, y1, r, size = createCoordinates(radius)
+        x1, y1, size = radius
         MyCircle(int(x1), int(y1), int(size), canvas)
 
 
@@ -53,7 +97,7 @@ def drawCurves(curves):
     receive all curves that are part of the main drawing and add them to the canvas
     '''
     for curve in curves:
-        x1, y1, x2, y2, x3, y3, x4, y4 = createCoordinates(curve)
+        x1, y1, x2, y2, x3, y3, x4, y4 = curve
         coordsMatrix = [[int(x1), int(y1)], [int(x2), int(y2)], [
             int(x3), int(y3)], [int(x4), int(y4)]]
         BezierCurve(coordsMatrix, canvas)
@@ -65,9 +109,12 @@ def readCoordinates():
     '''
     lines, radiuses, curves = selectFile()
 
+    lines, radiuses, curves = scaleInputToScreen(lines, radiuses, curves)
+
     drawLines(lines)
     drawRadiuses(radiuses)
     drawCurves(curves)
+
 
 def setUpGraphicalEnv():
     '''
