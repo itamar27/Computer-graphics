d0d4db27ea0fc1fb4dec79da37033771386b8f14
sivansalzmann <sivansalzmann10@gmail.com>
2021-06-13 00:23:25 +0300

    add buttons to GUI
    Need to fix:
    Perspective

--- EX3/GUI.py
+++ EX3/GUI.py
@@ -9,7 +9,6 @@ from advances_shape_utils import *
 class GUI:
     
     #Logical class variables
-    
 
     #UI elements
     width = 1100
@@ -21,7 +20,9 @@ class GUI:
     window.configure(background='SkyBlue3')
     canvas = Canvas(window, width=width-200, height=height, bg="white")
     helv36 = tkFont.Font(family='Helvetica', size=10, weight='bold')
-    
+
+    menuTop = Frame(window)
+
     def __init__(self):
         '''
         Initial GuI and logical elements of the program
@@ -30,7 +31,7 @@ class GUI:
         fileBtn = Button(self.window, text="Open file",  command= self.openFile,
                          height=6, width=30, bg='SkyBlue2', fg='white', font=self.helv36)
 
-        clearBtn = Button(self.window, text="Clear Screen",  # command=lambda: clearCanvas(canvas),
+        clearBtn = Button(self.window, text="Clear Screen",  command=lambda: self.canvas.delete('all'),
                           height=6, width=30, bg='SkyBlue2', fg='white', font=self.helv36)
 
         scaleBtn = Button(self.window, text="Scale",
@@ -43,15 +44,32 @@ class GUI:
                            height=6, width=30, bg='SkyBlue2', fg='white', font=self.helv36)
         self.msgText = Label(self.window, height=9, width=20, bg = "white")
 
+        # projections buttons
+        orthographic_btn = Button(self.menuTop, text="Orthographic",  command=lambda: self.draw("Orthographic"),
+                                  height=2, width=15, bg='SkyBlue2', fg='white', font=self.helv36)
+
+        oblique_btn = Button(self.menuTop, text="Oblique",  command=lambda: self.draw("Oblique"),
+                                 height=2, width=15, bg='SkyBlue2', fg='white', font=self.helv36)
+
+        perspective_btn = Button(self.menuTop, text="Perspective",  command=lambda: self.draw("Perspective"),
+                                 height=2, width=15, bg='SkyBlue2', fg='white', font=self.helv36)
+
         # packing all window elements
+        self.menuTop.pack(side=TOP)
         self.canvas.pack(side=RIGHT)
         fileBtn.pack(side=TOP)
         clearBtn.pack(side=TOP)
         scaleBtn.pack(side=TOP)
         rotateBtn.pack(side=TOP)
         helpBtn.pack(side=TOP)
+
+        # packing menu top
+        orthographic_btn.pack(side=LEFT)
+        oblique_btn.pack(side=LEFT)
+        perspective_btn.pack(side=LEFT)
+
         self.msgText.pack(side=TOP, pady= 10)
-        
+
 
     def presentMessage(self,msg=""):
         self.msgText['text'] = msg
@@ -62,18 +80,20 @@ class GUI:
     def openFile(self):
         coords, polygons = FileManager().openFile()
         self.data = Data(coords, polygons)
-        self.draw('Perspective')
+        self.draw('Orthographic')
 
 
     def draw(self,type_projection):
+        self.canvas.delete('all')
         polygons = self.data.getPolygons(type_projection)
         height_mid = self.height / 2
         width_mid = self.width / 2
-        for poly in polygons:
-            for i,p in enumerate(poly):
-                cord = []
-                cord.append(p[0] + width_mid)
-                cord.append(p[1] + height_mid)
-                poly[i] = tuple(cord)
 
+        for poly in polygons:
+            # for i,p in enumerate(poly):
+            #     cord = []
+            #     cord.append(p[0] + width_mid)
+            #     cord.append(p[1] + height_mid)
+            #     poly[i] = tuple(cord)
+            self.presentMessage("Projection type: {}\n".format(type_projection))
             self.canvas.create_polygon(poly,fill='#ffffff',outline='#000000')
--- EX3/advances_shape_utils.py
+++ EX3/advances_shape_utils.py
@@ -23,25 +23,19 @@ class Data:
     def sortPolygons(self):
         self.polygons.sort(key=lambda poly: poly.depth, reverse=True)
 
-        for poly in self.polygons:
-            print(poly.coords)         
-        
-        
-
     def getPolygons(self,type_projection):
         polygons = []
         if type_projection == 'Orthographic':
             for poly in self.polygons:
                 polygons.append(poly.orthographicCoords())
-        if type_projection == 'Oblique':
+        elif type_projection == 'Oblique':
             for poly in self.polygons:
                 polygons.append(poly.obliqueCoords())
-        if type_projection == 'Perspective':
+        elif type_projection == 'Perspective':
             for poly in self.polygons:
+                print(poly.coords)
                 polygons.append(poly.perspectiveCoords())
         return polygons
-
-        
 
 class Polygon:
     def __init__(self, coords):
@@ -87,13 +81,12 @@ class Polygon:
             new_point = [point[0],point[1],point[2],1]
             coords = np.matmul(new_point, ortographicMatrix)
             coordsOrthographic.append((coords[0],coords[1]))
-
         return coordsOrthographic
 
     def obliqueCoords(self):
         coordsOblique = []
-        v1 = 0.5 * math.cos(30 * math.pi / 180)
-        v2 = 0.5 * math.sin(30 * math.pi / 180)
+        v1 = -0.5 * math.cos(30 * math.pi / 180)
+        v2 = -0.5 * math.sin(30 * math.pi / 180)
         obliqueMatrix = [[1, 0, 0, 0],
                           [0, 1, 0, 0],
                           [v1, v2, 1, 0],
@@ -111,19 +104,17 @@ class Polygon:
         D = 350
 
         for point in self.coords:
-            sz = D / (point[2] + D)
+            sz = D / (int(point[2]) + D)
             perspectiveMatrix = [[sz, 0, 0, 0],
                                  [0, sz, 0, 0],
                                  [0, 0, 0, 0],
                                  [0, 0, 0, 1]]
-            new_point = [point[0],point[1],point[2],1]
+            new_point = [int(point[0]),int(point[1]),int(point[2]),1]
             coords = np.matmul(new_point, perspectiveMatrix)
-            coordsPerspective.append((coords[0],coords[1]))
-
+            coordsPerspective.append((int(coords[0]),int(coords[1])))
         return coordsPerspective
 
     def minMaxValues(self):
-        
         # minMaxes will be filled with: [min x, max x, min y, max y, min z, max z]
         minMaxes = [] 
         xVals = []
